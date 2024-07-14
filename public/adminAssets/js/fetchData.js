93216ecbfdcf8f7c02240d5cf693fd431325724c
Ajil VC <vcajiltvr@gmail.com>
2024-07-14 18:18:02 +0530

    Completed Add new Product validation

--- public/adminAssets/js/fetchData.js
+++ public/adminAssets/js/fetchData.js
@@ -328,7 +328,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let validation = false;
     let alreadyExistName = false;
-    function isProductExist(){
+    async function isProductExist(productExist){
         
         product_name_error.classList.add('d-none');
                 product_name_error.textContent = ""
@@ -341,32 +341,37 @@ document.addEventListener('DOMContentLoaded', function() {
             validation = false;
           
         }else{
-    
-            fetch(`http://localhost:2000/admin/productslist/add_new_product?product_name=${newValue.trim()}`,{method : 'post'})
-            .then(response => {
+            
+            try{
+
+                const response = await fetch(`http://localhost:2000/admin/productslist/add_new_product?product_name=${newValue.trim()}`,{method : 'post'});
                 if(!response.ok){
-                    throw new Error("Network response was not ok while fetching data.");
+                    throw new Error("Network response was not ok while fetching data to check product is exist.");
                 }
-                return response.json();
-            })
-            .then(data =>{
     
+                const data = await response.json();
                 if(!data.status){
                    
                     product_name_error.classList.remove('d-none');
                     product_name_error.textContent = data.message
                     validation = false;
                     alreadyExistName = true;
-       
+                    
+                    console.log(productExist,"This Product exist value in function jjj")
+                    if(productExist){
+                        return true;
+                    }
                 }
-                             
-            })
+            }
+            catch(error){
+                console.log("Error while trying to fetch data to check if product is there.",error);
+            }
         }
     }
     if(product_name){
         product_name.addEventListener('input',()=> {
 
-            isProductExist();
+            isProductExist(0);
         })
     }
     
@@ -378,6 +383,176 @@ document.addEventListener('DOMContentLoaded', function() {
     const targetGroup_error = document.getElementById('targetGroup-error');
     const category_error = document.getElementById('category-error');
     const brand_error = document.getElementById('brand-error');
+    const image_error = document.getElementById('image-error');
+
+    const publishNewProduct = async()=>{
+        
+            const isProduct = await isProductExist(1);
+            
+            if(!isProduct){
+                console.log(imageBlobs,"This is from !isProduct ha ok")
+                let formValidation = true;
+                regularPrice_error.textContent = "";
+                salePrice_error.textContent = "";
+                stockQuantity_error.textContent = "";
+                product_name_error.textContent = "";
+                descriptionOfProduct_error.textContent = "";
+                targetGroup_error.textContent = "";
+                category_error.textContent = "";
+                brand_error.textContent = "";
+                image_error.textContent = "";
+                product_name_error.classList.add('d-none');
+    
+                let regularPrice = document.getElementById('regularPrice').value.trim();
+                let salePrice = document.getElementById('salePrice').value.trim();
+                let stockQuantity = document.getElementById('stockQuantity').value.trim();
+                let productName = product_name.value.trim();
+                let descriptionOfProduct = document.getElementById('descriptionOfProduct').value.trim();
+                let targetGroup = document.getElementById('targetGroup').value;
+                let category = document.getElementById('category').value;
+                let brand = document.getElementById('brand').value;
+                
+                if(Object.keys(imageBlobs).length === 0){
+                    image_error.textContent = "Select Images";
+                    formValidation = false;
+                }else if(Object.keys(imageBlobs).length < 3){
+                    image_error.textContent = "Minimum 3 images";
+                    formValidation = false;
+                }
+                if(!brand){
+                    brand_error.textContent = "Select brand";
+                    formValidation = false;
+                }
+                if(!category){
+                    category_error.textContent = "Select category";
+                    formValidation = false;
+                }
+                if(!targetGroup){
+                    targetGroup_error.textContent = "Select target group";
+                    formValidation = false;
+                }
+                if(!/^[1-9]\d*$/.test(regularPrice)){
+                    
+                    regularPrice_error.textContent = "Must be a positive number.";
+                    formValidation = false;
+                }
+                if(!/^[1-9]\d*$/.test(salePrice)){
+                    
+                    salePrice_error.textContent = "Must be a positive number.";
+                    formValidation = false;
+                }
+                if(!/^[1-9]\d*$/.test(stockQuantity)){
+    
+                    stockQuantity_error.textContent = "Must be a positive number.";
+                    formValidation = false;
+                }
+                if(productName === ""){
+                    
+                    product_name_error.textContent = "This field should not be empty.";
+                    product_name_error.classList.remove('d-none');
+                    formValidation = false;
+                }else if(alreadyExistName){
+    
+                    product_name_error.classList.remove('d-none');
+                    product_name_error.textContent = "A product is already exist in this name"
+                }
+    
+                if(descriptionOfProduct === ""){
+                    
+                    descriptionOfProduct_error.textContent = "Please type something here";
+                    formValidation = false;
+                }
+                
+    
+              
+                if(validation && formValidation && !alreadyExistName){
+    
+                    console.log(validation,"From publish working")
+    
+    
+                    const category = document.getElementById('category').value;
+                    const brand = document.getElementById('brand').value;
+                  
+                    
+                    Array.from(form.elements).forEach(element => {
+        
+                        if(element.name && element.type !== 'file'){
+                            formDataForAddNewProduct.append(element.name, element.value);
+                        }
+                    })
+        
+        
+                    const hiddenCategory = document.createElement('input');
+                    const hiddenBrand = document.createElement('input');
+        
+                    
+                    //Adding the hidden field to the form.
+                    hiddenCategory.type = 'hidden';
+                    hiddenCategory.name = 'category';
+                    hiddenCategory.value = category;
+                    
+                    hiddenBrand.type = 'hidden';
+                    hiddenBrand.name = 'brand';
+                    hiddenBrand.value = brand;
+                    
+                    formDataForAddNewProduct.append(hiddenCategory.name, hiddenCategory.value);
+                    formDataForAddNewProduct.append(hiddenBrand.name, hiddenBrand.value);
+                    
+                    Object.entries(imageBlobs).forEach(([key, blob], index) => {
+                        formDataForAddNewProduct.append('image', blob, `croppedimage${index + 1}.png`);
+                    });
+                   
+                    try{
+
+                        const response = await fetch(`http://localhost:2000/admin/productslist/add_new_product`,{
+                            method : 'post',
+                            body : formDataForAddNewProduct
+                        });
+    
+                        if(!response.ok){
+        
+                            Swal.fire({
+                                title: 'Error!',
+                                text: "Something Went wrong",
+                                icon: 'error'
+                            });
+                            throw new Error('Network response was not ok in submission of new product.')
+                        }
+                        const data = await response.json();
+                        if(!data.status){
+    
+                            Swal.fire({
+                                title: 'Error!',
+                                text: data.message,
+                                icon: 'error'
+                            });
+    
+                        }else{
+        
+                            Swal.fire({
+                                title: '',
+                                text: data.message,
+                                icon: 'success'
+                            });
+                        }
+                    }
+                    catch(error){
+                        console.log("There was a problem with submission of add new pRoduct operation",error)
+                    };
+                    
+                }
+            }else{
+                
+                Swal.fire({
+                    title: 'Error!',
+                    text: "Product is Already added!!",
+                    icon: 'error'
+                });
+            }
+
+           
+    }
+
 
     const publishBtnForAddProduct = document.getElementById('publishBtnForAddProduct');
     if(publishBtnForAddProduct){
@@ -385,153 +560,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         publishBtnForAddProduct.addEventListener('click',() => {
             
-            const isProduct = isProductExist();
-            console.log(isProduct,"Checking if product is there")
-            let formValidation = true;
-            regularPrice_error.textContent = "";
-            salePrice_error.textContent = "";
-            stockQuantity_error.textContent = "";
-            product_name_error.textContent = "";
-            descriptionOfProduct_error.textContent = "";
-            targetGroup_error.textContent = "";
-            category_error.textContent = "";
-            brand_error.textContent = "";
-            product_name_error.classList.add('d-none');
-
-            let regularPrice = document.getElementById('regularPrice').value.trim();
-            let salePrice = document.getElementById('salePrice').value.trim();
-            let stockQuantity = document.getElementById('stockQuantity').value.trim();
-            let productName = product_name.value.trim();
-            let descriptionOfProduct = document.getElementById('descriptionOfProduct').value.trim();
-            let targetGroup = document.getElementById('targetGroup').value;
-            let category = document.getElementById('category').value;
-            let brand = document.getElementById('brand').value;
-
-            if(!brand){
-                brand_error.textContent = "Select brand";
-                formValidation = false;
-            }
-            if(!category){
-                category_error.textContent = "Select category";
-                formValidation = false;
-            }
-            if(!targetGroup){
-                targetGroup_error.textContent = "Select target group";
-                formValidation = false;
-            }
-            if(!/^[1-9]\d*$/.test(regularPrice)){
-                
-                regularPrice_error.textContent = "Must be a positive number.";
-                formValidation = false;
-            }
-            if(!/^[1-9]\d*$/.test(salePrice)){
-                
-                salePrice_error.textContent = "Must be a positive number.";
-                formValidation = false;
-            }
-            if(!/^[1-9]\d*$/.test(stockQuantity)){
-
-                stockQuantity_error.textContent = "Must be a positive number.";
-                formValidation = false;
-            }
-            if(productName === ""){
-                
-                product_name_error.textContent = "This field should not be empty.";
-                product_name_error.classList.remove('d-none');
-                formValidation = false;
-            }else if(alreadyExistName){
-
-                product_name_error.classList.remove('d-none');
-                product_name_error.textContent = "A product is already exist in this name"
-            }
-
-            if(descriptionOfProduct === ""){
-                
-                descriptionOfProduct_error.textContent = "Please type something here";
-                formValidation = false;
-            }
-            
-
-          
-            if(validation && formValidation && !alreadyExistName){
-
-                console.log(validation,"From publish working")
-
-
-                const category = document.getElementById('category').value;
-                const brand = document.getElementById('brand').value;
-              
-                
-                Array.from(form.elements).forEach(element => {
-    
-                    if(element.name && element.type !== 'file'){
-                        formDataForAddNewProduct.append(element.name, element.value);
-                    }
-                })
-    
-    
-                const hiddenCategory = document.createElement('input');
-                const hiddenBrand = document.createElement('input');
-    
-                
-                //Adding the hidden field to the form.
-                hiddenCategory.type = 'hidden';
-                hiddenCategory.name = 'category';
-                hiddenCategory.value = category;
-                
-                hiddenBrand.type = 'hidden';
-                hiddenBrand.name = 'brand';
-                hiddenBrand.value = brand;
-                
-                formDataForAddNewProduct.append(hiddenCategory.name, hiddenCategory.value);
-                formDataForAddNewProduct.append(hiddenBrand.name, hiddenBrand.value);
-                
-                Object.entries(imageBlobs).forEach(([key, blob], index) => {
-                    formDataForAddNewProduct.append('image', blob, `croppedimage${index + 1}.png`);
-                });
-               
-                
-                fetch(`http://localhost:2000/admin/productslist/add_new_product`,{
-                    method : 'post',
-                    body : formDataForAddNewProduct
-                })
-                .then(response => {
-                    if(!response.ok){
-    
-                        Swal.fire({
-                            title: 'Error!',
-                            text: "Something Went wrong",
-                            icon: 'error'
-                        });
-                        throw new Error('Network response was not ok in submission of new product.')
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    
-                    if(!data.status){
-
-                        Swal.fire({
-                            title: 'Error!',
-                            text: data.message,
-                            icon: 'error'
-                        });
-
-                    }else{
-    
-                        Swal.fire({
-                            title: '',
-                            text: data.message,
-                            icon: 'success'
-                        });
-                    }
-                                     
-                })
-                .catch(error => {
-                    console.log("There was a problem with submission of add new pRoduct operation",error)
-                });
-        
-            }
+            publishNewProduct();
 
         })
     
